2cfac1fcfc6e9b9f8524a38775277c6ac02803ce
simillakoushik <simillakoushik@gmail.com>
2021-07-14 13:14:41 +0530

    webhook trigger check

--- src/main/java/com/myapp/spring/NewDevopsSpringBootDemoApplication.java
+++ src/main/java/com/myapp/spring/NewDevopsSpringBootDemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+/*
+ * 
+ * some changes made
+ */
 @SpringBootApplication(	)
 public class NewDevopsSpringBootDemoApplication {
 
